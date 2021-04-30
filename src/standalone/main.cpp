c9c306e79cd988f53e545a2082c164ffb2f23bf9
Mark Chigrin <dev@chigrin.ml>
2021-04-30 06:15:22 +0000

    meh

--- src/standalone/main.cpp
+++ src/standalone/main.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
 
-#include "../lib/avlTree.hpp"
+#include "../lib/lib.hpp"
 
 using namespace Algorithms;
 
-int main() {
-    auto t = new Tree<std::string>();
-    std::string input;
-    while (std::cin >> input && input != "0") {
-        t->insert(input);
+enum Color { white, grey, black };
+
+template <size_t N>
+using Matrix = std::array<std::array<size_t, N>, N>;
+
+template<size_t N>
+void dfs(size_t i, const Matrix<N> &matrix, std::array<Color, N> &color, std::vector<size_t> &sorted) {
+    switch (color[i])
+    {
+        case black:
+            return;
+
+        case grey:
+            throw std::invalid_argument("Cycle in input data. Can't sort");
+
+        case white:
+            color[i] = grey;
+            
+            for (size_t to = 0; to < N; to++)
+            {
+                auto weight = matrix[i][to];
+                if(weight != 0){
+                    dfs(to, matrix, color, sorted);
+                }
+            }
+
+            color[i] = black;
+            sorted.push_back(i);
     }
-        
-    t->for_each( [] (std::string el) {
-            std::cout << el << " ";
-        }
-    );
+}
+
+template<size_t N>
+std::vector<size_t> sort(const Matrix<N> &matrix) {
+    std::array<Color, N> color = { white };
+    std::vector<size_t> sorted;
+    for (size_t i = 0; i < N; i++)
+    {
+       dfs(i, matrix, color, sorted);
+    }
+    std::reverse(sorted.begin(), sorted.end());
+    return sorted;
+}
+
+int main() {
+    Matrix<4> matrix = {{
+        {0, 0, 0, 1},
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0}
+    }};
+    for (auto el : sort(matrix))
+    {
+        std::cout << el + 1 << " ";    
+    }   
     std::cout << std::endl;
 }
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,38 +1,13 @@
 #include <boost/ut.hpp>
 #include <string>
 
-#include "../lib/avlTree.hpp"
+#include "../lib/lib.hpp"
 
 using namespace boost::ut;
 using namespace Algorithms;
 
 int main() {
     "simple"_test = [] {
-        auto t = new Tree<int>();
-        t->insert(2);
-        t->insert(1);
-        t->insert(3);
-        int i = 1;
-        t->for_each([&] (int x) {
-            expect(x == i);
-            i += 1;
-        });
     };
-    "contains"_test = [] {
-        auto t = new Tree<int>();
-        t->insert(1);
-        t->insert(2);
-        expect(t->contains(1));
-        expect(!t->contains(3));
-    };
-    "order"_test = [] {
-        auto t = new Tree<std::string>();
-        t->insert("abv");
-        t->insert("aqv");
-        t->insert("cgd");
-        t->insert("fdx");
-        t->insert("qwr");
-        t->insert("aaa");
-        t->insert("abc");
-    };
+
 }
